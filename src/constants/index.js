fdc9ac7dc3bfc847402032bac2cdda11b7dd8c8a
Peter Revesz <reveszpeter21@gmail.com>
2023-03-11 21:20:34 +0100

    edit constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -166,26 +166,26 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
+      "Testimonal 1",
+    name: "Mentor",
+    designation: "Mentor",
+    company: "CC",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
+      "Testimonal 2",
+    name: "Mentor",
+    designation: "Mentor",
+    company: "CC",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
+      "Testimonal 3",
+    name: "Lead Dev",
+    designation: "Lead Dev",
+    company: "CC",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
